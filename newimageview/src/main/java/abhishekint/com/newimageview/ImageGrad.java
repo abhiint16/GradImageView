51f579cab700e18cbec69c1429f8c659b8539f17
Abhishek Kumar <abhishekint16@gmail.com>
2018-04-19 12:58:30 +0530

    linking lib to the project and init java

---
+++ newimageview/src/main/java/abhishekint/com/newimageview/ImageGrad.java
@@ -0,0 +1,27 @@
+package abhishekint.com.newimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by abhishek on 19-04-2018.
+ */
+
+public class ImageGrad extends RelativeLayout {
+    public ImageGrad(Context context) {
+        super(context);
+    }
+
+    public ImageGrad(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageGrad(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ImageGrad(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}

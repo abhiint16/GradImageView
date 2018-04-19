117c4e9408211f4fb64409b0eb26ef780c9928b5
Abhishek Kumar <abhishekint16@gmail.com>
2018-04-19 13:09:17 +0530

    adding core items and initialising from constructor

--- newimageview/src/main/java/abhishekint/com/newimageview/ImageGrad.java
+++ newimageview/src/main/java/abhishekint/com/newimageview/ImageGrad.java
@@ -2,6 +2,7 @@ package abhishekint.com.newimageview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 /**
@@ -9,16 +10,28 @@ import android.widget.RelativeLayout;
  */
 
 public class ImageGrad extends RelativeLayout {
+    /** Core Items*/
+    private Context mContext;
+    private AttributeSet attrs;
+    private int styleAttr;
+    private View view;
+
     public ImageGrad(Context context) {
         super(context);
+        this.mContext=context;
     }
 
     public ImageGrad(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.mContext=context;
+        this.attrs=attrs;
     }
 
     public ImageGrad(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.mContext=context;
+        this.attrs=attrs;
+        this.styleAttr=defStyleAttr;
     }
 
 }
